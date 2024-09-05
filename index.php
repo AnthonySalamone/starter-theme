11d9a74959a0042eb23745a8dfaf17e2cc4a4ba0
Anthony Salamone <anthonysalamone613@gmail.com>
2024-09-05 20:06:56 +0200

    fixing

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 /**
  * The main template file
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  */
 
@@ -16,16 +11,14 @@ use Timber\Timber;
 $templates = array('templates/index.twig');
 
 if (is_home()) {
-	array_unshift($templates, 'templates/front-page.twig', 'templates/home.twig');
+    array_unshift($templates, 'templates/front-page.twig', 'templates/home.twig');
 }
 
 // Récupérer le contexte global sans argument
 $context = Timber::context();
 
-// Ajouter des variables supplémentaires au contexte si nécessaire
-$context['foo'] = 'bar';
-$context['posts'] = new Timber\PostQuery(); // Récupérer les posts
+// Ajouter les posts au contexte
+$context['posts'] = new Timber\PostQuery(); // Récupérer la requête par défaut des posts
 
 // Rendu du modèle Twig avec le contexte
 Timber::render($templates, $context);
-
